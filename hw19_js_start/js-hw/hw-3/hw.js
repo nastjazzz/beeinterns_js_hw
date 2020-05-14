48d542ab806d450b12ca9a41bb254c200af765bd
Nastya Zamashnyuk <hhobert@student.21-school.ru>
2020-05-14 22:22:41 +0300

    change variable

--- hw19_js_start/js-hw/hw-3/hw.js
+++ hw19_js_start/js-hw/hw-3/hw.js
@@ -6,23 +6,12 @@
     следуя рекомендациям по именованию переменных
 */
 
-// var age = 20;
-// var text = 'Ваш возраст: ' + age + ' лет';
-// alert(text); // Должен вывести: Ваш возраст: 20 лет
-
-let age = 20;
-let text = `Ваш возраст: ${age} лет`;
+const age = 20;
+const text = `Ваш возраст: ${age} лет`;
 alert(text);
 
-// var hours = 12;
-// var minutes = 45;
-// hours = hours + 1;
-// var textwithtime = 'Текущее время: ' + hours + ':' + minutes;
-//
-// alert(textwithtime); // Должен вывести: Текущее время: 13:45
-
 let hours = 12;
-let minutes = 45;
+const minutes = 45;
 hours += 1;
 let textWithTime = `Текушее время: ${hours}:${minutes}`;
 alert(textWithTime);
