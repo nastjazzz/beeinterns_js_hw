57052833540f3e3ac397942cb4216809fb5a7bad
Nastya <hhobert@student.21-school.ru>
2020-06-30 18:39:38 +0300

    edit hw

--- hw_29/script.js
+++ hw_29/script.js
@@ -1,10 +1,18 @@
 const form = document.querySelector('.form');
-const keyName = 'savedCookie';
+const keyName = 'savedCookie'; //key LocalStorage
+const cookieName = 'cookie'; //name cookie
 let objectData;
+let valueCookie; //значение куки
 
 document.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.length) {
+    if (checkCookieName(cookieName)) {
+        console.log('восстанавливаю из куки');
         recoverValue();
+    } else if (localStorage.length) {
+        console.log('восстанавливаю из localStorage');
+        recoverValue();
+    } else {
+        console.log('нечего восстанавливать');
     }
 });
 
@@ -12,13 +20,33 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     objectData = Object.fromEntries(formData.entries());
-    localStorage.setItem(keyName, JSON.stringify(objectData));
+
+// ДЛЯ ПРОВЕРКИ РАБОТОСПОСОБНОСТИ СОХРАНЕНИЯ/ВОССТАНОВЛЕНИЯ ИН-ЦИИ Я КОММИТИЛА ЛИБО 1Й, ЛИБО 2Й ВАРИАНТ
+// ЕСЛИ ОБА ВАРИАНТА РАСКОММИЧЕНЫ, ТО БУДУТ РАБОТАТЬ ОБА, НО ВОССТАНОВЛЕНИЕ ПОЙДЕТ ИЗ КУКИ
+
+// 1) СОХРАНЕНИЕ ИН-ЦИИ В КУКИ
+    let str = JSON.stringify(objectData);
+    document.cookie = `${cookieName}=${str}`;
+
+// 2) СОХРАНЕНИЕ ИН-ЦИИ В LOCALSTORAGE
+    // localStorage.setItem(keyName, JSON.stringify(objectData));
 })
 
-function recoverValue() {
-    const objCookie = JSON.parse(localStorage.getItem(keyName))
 
-    for (let i = 0; i < 6; i++) {
+function checkCookieName(name) {
+    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) {
+        valueCookie = match[2];
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function recoverValue() {
+    let objCookie = checkCookieName(cookieName) ? JSON.parse(valueCookie) : JSON.parse(localStorage.getItem(keyName));
+
+	for (let i = 0; i < 6; i++) {
         if (objCookie[form[i].name] && form[i].type !== 'checkbox') {
             form[i].value = objCookie[form[i].name];
         } else if (objCookie[form[i].name] && form[i].type === 'checkbox') {
@@ -28,5 +56,3 @@ function recoverValue() {
         }
     }
 }
-
-
